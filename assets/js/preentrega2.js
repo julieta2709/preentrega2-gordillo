a91474ac251da12d45ac6251c38105d2da7bd840
Julieta Gordillo <julietagordillo13@hotmail.com>
2022-10-07 12:28:07 -0300

    empiezo a construir codigo js

--- assets/js/preentrega2.js
+++ assets/js/preentrega2.js
@@ -1,4 +1,17 @@
-function Equipo (pais, figuritas){
-this.pais = pais
-this.figuritas = figuritas
+let pais = prompt('Ingrese el nombre del país')
+let figuritas = parseint(prompt('Ingrese la cantidad de figuritas'))
+
+
+
+class Equipo {
+    constructor(pais, figuritas) {
+        this.pais = function () {
+            return pais;
+        };
+
+        this.figuritas = function () {
+            return figuritas;
+        };
+    }
 }
+
