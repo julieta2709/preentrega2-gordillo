94e84704a223a5ba051f2b2cdccb21428f2175e4
Julieta Gordillo <julietagordillo13@hotmail.com>
2022-10-08 14:07:36 -0300

    trabajo con js

--- assets/js/preentrega2.js
+++ assets/js/preentrega2.js
@@ -1,11 +1,9 @@
 let pais = prompt('Ingrese el nombre del país')
 let figuritas = parseInt(prompt('Ingrese la cantidad de figuritas', 0));
 
-const paisesmundial = ["Argentina", "Brasil", "HOlanda", "Iran", "Korea del sur", "Japón", "Arabia Saudita", "Ecuador", "Uruguay", "Canada", "Senegal", "Ghana", "Tunes", "Camerún", "Marruecos", "Polonia", "Portugal", "México", "Estados Unidos", "Gales", "Australia", "Costa Rica", "Dinamarca", "Alemania", "Bélgica", "Francia", "Croacia", "Serbia", "España", "Inglaterra", "Suiza", "Qatar"]
+/* const paisesmundial = ["Argentina", "Brasil", "Holanda", "Iran", "Korea del sur", "Japón", "Arabia Saudita", "Ecuador", "Uruguay", "Canada", "Senegal", "Ghana", "Tunes", "Camerún", "Marruecos", "Polonia", "Portugal", "México", "Estados Unidos", "Gales", "Australia", "Costa Rica", "Dinamarca", "Alemania", "Bélgica", "Francia", "Croacia", "Serbia", "España", "Inglaterra", "Suiza", "Qatar"] */
 const totalfigus = 670
 const totalfigusequipo = 19
-
-
 
 function verificar(numero) {
 
@@ -16,7 +14,7 @@ function verificar(numero) {
             mensaje = alert("El numero está dentro del rango");
         } else {
             while (figuritas > totalfigusequipo) {
-                mensaje = alert("El numero está fuera de rango, el valor ingresado tiene que ser menor que 19")
+                mensaje = alert("El numero está fuera de rango, el valor ingresado tiene que ser menor o igual que 19")
                 figuritas = parseInt(prompt('Ingrese la cantidad de figuritas', 0))
             }
         }
@@ -29,41 +27,24 @@ function verificar(numero) {
 }
 verificar(figuritas)
 
+/* let paisesingresados = []
+paisesingresados[paisesingresados.length] = pais
+console.log(`Ha ingresado el país: ${pais}`)
+console.log(paisesingresados) */
+
 class Equipo {
     constructor(pais, figuritas) {
-        this.pais = function () {
-            return pais;
-        };
-
-        this.figuritas = function () {
-            return figuritas || [];
-        };
-    }
-
-    getPais() {
-        return pais();
-    }
-
-    getFiguritas() {
-        return figuritas();
-    }
-
-    setPais(nuevo_pais) {
-        this.pais = function () {
-            return nuevo_pais;
-        };
-    }
-    setfiguritas(nuevas_figuritas) {
-        this.figuritas = function () {
-            return nuevas_figuritas;
-        }
-    }
-
-    getValorFiguritasFaltantes() {
-        let figuritasfaltanes = []
-        this.pais().forEach(function (elemento, index) {
-            getValorFiguritasFaltantes.push(elemento.figuritas());
-        });
-        console.log(`Cantidad de figuritas faltantes del país: ${elemento.pais ()}`, valorFiguritasFaltantes.reduce((totalfigusequipo, figuritas) => totalfigusequipo - figuritas, 0))
-    }
+        this.pais = pais;
+        this.figuritas = figuritas;
+    };
 }
+const equipo = new Equipo(pais, figuritas);
+
+let restanfiguritas = totalfigusequipo - figuritas
+
+const paisesingresados = (equipo) => {
+    equipo.push(pais);
+};
+console.log (`Ha ingresado el país: ${pais}`)
+console.log (`Le faltan: ${restanfiguritas} para completar el equipo`)
+
