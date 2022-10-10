95a0b3566d28b99f6648d72f284da1485e5812be
Julieta Gordillo <julietagordillo13@hotmail.com>
2022-10-10 00:52:50 -0300

    termino código js

--- assets/js/preentrega2.js
+++ assets/js/preentrega2.js
@@ -1,50 +1,88 @@
-let pais = prompt('Ingrese el nombre del país')
-let figuritas = parseInt(prompt('Ingrese la cantidad de figuritas', 0));
+let cantidadpaises = prompt('Ingrese mediante un número entero la cantidad de paises que va a ingresar');
 
-/* const paisesmundial = ["Argentina", "Brasil", "Holanda", "Iran", "Korea del sur", "Japón", "Arabia Saudita", "Ecuador", "Uruguay", "Canada", "Senegal", "Ghana", "Tunes", "Camerún", "Marruecos", "Polonia", "Portugal", "México", "Estados Unidos", "Gales", "Australia", "Costa Rica", "Dinamarca", "Alemania", "Bélgica", "Francia", "Croacia", "Serbia", "España", "Inglaterra", "Suiza", "Qatar"] */
-const totalfigus = 670
+
 const totalfigusequipo = 19
-
-function verificar(numero) {
-
-    figuritas = numero
-
-    if (!isNaN(figuritas)) {
-        if (figuritas <= totalfigusequipo) {
-            mensaje = alert("El numero está dentro del rango");
-        } else {
-            while (figuritas > totalfigusequipo) {
-                mensaje = alert("El numero está fuera de rango, el valor ingresado tiene que ser menor o igual que 19")
-                figuritas = parseInt(prompt('Ingrese la cantidad de figuritas', 0))
-            }
-        }
-
-    } else {
-        mensaje = alert("Debes ingresar sólo números")
-        figuritas = parseInt(prompt('Ingrese la cantidad de figuritas', 0))
-        verificar(figuritas)
-    }
-}
-verificar(figuritas)
-
-/* let paisesingresados = []
-paisesingresados[paisesingresados.length] = pais
-console.log(`Ha ingresado el país: ${pais}`)
-console.log(paisesingresados) */
+let contador = 0
+let listapaises = []
 
 class Equipo {
     constructor(pais, figuritas) {
-        this.pais = pais;
-        this.figuritas = figuritas;
+        this._pais = function () {
+            return pais;
+        };
+        this._figuritas = function () {
+            return figuritas;
+        };
+    }
+    getPais() {
+        return this._pais();
+    }
+    setPais(nuevo_pais) {
+        this._pais = function () {
+            return nuevo_pais;
+        };
+    }
+    getFiguritas() {
+        return this._figuritas;
     };
+    setFiguritas(nueva_figuritas) {
+        this._figuritas = function () {
+            return nueva_figuritas;
+        };
+    }
+    getEquipos() {
+        this._pais().forEach(element => {
+            console.log(`País ingresado: ${element._pais()}`);
+            console.log(`Cantidad de figuritas: ${element._figuritas()}`);
+        });
+    }
+    getCantTotalFiguritas() {
+        let cantfiguritas = []
+        this._pais().forEach(element => {
+            cantfiguritas.push(element._figuritas());
+        });
+        console.log('Cantidad total de figuritas', cantfiguritas.reduce((valorAnterior, valorActual) => valorAnterior + valorActual, 0))
+    }
+
 }
-const equipo = new Equipo(pais, figuritas);
+while (contador < cantidadpaises) {
+    let pais = prompt('Ingrese el nombre del País')
+    let figuritas = parseInt(prompt('Ingrese la cantidad de figuritas'))
 
-let restanfiguritas = totalfigusequipo - figuritas
+    function verificar(numero) {
 
-const paisesingresados = (equipo) => {
-    equipo.push(pais);
-};
-console.log (`Ha ingresado el país: ${pais}`)
-console.log (`Le faltan: ${restanfiguritas} para completar el equipo`)
+        figuritas = numero
 
+        if (!isNaN(figuritas)) {
+            if (figuritas <= totalfigusequipo) {
+                mensaje = alert("El numero está dentro del rango");
+            } else {
+                while (figuritas > totalfigusequipo) {
+                    mensaje = alert("El numero está fuera de rango, el valor ingresado tiene que ser menor o igual que 19")
+                    figuritas = parseInt(prompt('Ingrese la cantidad de figuritas', 0))
+                }
+            }
+
+        } else {
+            mensaje = alert("Debes ingresar sólo números")
+            figuritas = parseInt(prompt('Ingrese la cantidad de figuritas', 0))
+            verificar(figuritas)
+        }
+    }
+    verificar(figuritas)
+
+
+    let equipo = new Equipo(pais, figuritas)
+    listapaises.push(equipo)
+
+    contador++
+}
+
+const imprimirEquipo = (equipo) => {
+    equipo.getEquipos();
+    equipo.getCantTotalFiguritas();
+}
+
+let equipo = new Equipo(listapaises)
+
+imprimirEquipo(equipo, listapaises)
